ce1692c95fc53e6d882402782cbccfa9ecc989ef
Iara <123iamela@gmail.com>
2018-08-15 11:49:48 -0300

    Actualización

--- R/Proportion.R
+++ R/Proportion.R
@@ -1,4 +1,19 @@
-#APERTURA ARCHIVO
+#' APERTURA ARCHIVO
+
+#' Lee archivos que contengan el habitat y grupo funcional de las especies de uno o varios ecosistemas en formato .csv o separados por tabulaciones en columnas
+#'
+#' @param fileName un archivo con el habitat y grupo funcional de cada especie
+#'
+#' @return un data frame
+#' @export
+#'
+#' @examples
+#'
+#'# Lee archivo con ecosistema, habitat y grupo funcional separado en columnas por tabulaciones
+#'
+#'fileName <- system.file("exdata", "traits.txt", package = "Proporciones")
+#'tabla <- readFile(fileName)
+
 readFile<- function(fileName) {
 
   data <- read.delim(fileName, stringsAsFactors = FALSE)
@@ -6,7 +21,25 @@ readFile<- function(fileName) {
   return(data)
 }
 
-#CÁLCULO DE LAS PROPORCIONES DE HÁBITATS Y GRUPOS FUNCIONALES
+#' CÁLCULO DEL TOTAL DE ESPECIES Y PROPORCIONES POR HÁBITATS Y GRUPOS FUNCIONALES
+
+#' @param data un data frame
+#'
+#' @return una tabla con los siguientes campos:
+#'         Name: nombre del ecosistema
+#'         Habitat/FunctionalGroup: categorias dentro de habitat/grupo funcional
+#'         n: total de especies por categoría
+#'         propH/propFG: proporción de especies por categoria
+#'
+#' @export
+#' @importFrom dplyr    group_by %>% summarise mutate
+#'
+#' @examples
+#'
+#' Habitat <- pH(tabla)
+#' FunctionalGroup <- pFG(tabla)
+
+#' PARA HÁBITATS
 pH <- function(data) {
 
   prop <- data %>% group_by(Name,Habitat) %>% summarize(n= n()) %>% mutate(propH=n/sum(n))
@@ -14,11 +47,7 @@ pH <- function(data) {
   return(prop)
 }
 
-tablapH<- function(pH){
-
-  return(kable(pH, caption = "Habitat by Site"))
-}
-
+#' PARA GRUPOS FUNCIONALES
 pFG <- function(data) {
 
   prop <- data %>% group_by(Name,FunctionalGroup) %>% summarize(n= n()) %>% mutate(propFG=n/sum(n))
@@ -26,13 +55,23 @@ pFG <- function(data) {
   return(prop)
 }
 
-tablapFG <- function(pFG) {
 
-  return(kable(pFG, caption = "Functional Group by Site"))
-}
+#'GRÁFICO DE LAS PROPORCIONES DE HÁBITATS Y GRUPOS FUNCIONALES
 
+#'@param pH un data frame
+#'@param pFG un data frame
+#'
+#' @return un gráfico
+#'
+#' @export
+#' @importFrom ggplot2    ggplot geom_bar theme_bw scale_fill_brewer
+#'
+#' @examples
+#'
+#' plotpHabitat(Habitat)
+#' plotpFG(FunctionalGroup)
 
-#GRÁFICO DE LAS PROPORCIONES DE HÁBITATS Y GRUPOS FUNCIONALES
+#' PARA HÁBITATS
 plotpHabitat <- function(pH) {
 
   plot <- ggplot(pH, aes(x=factor(Habitat), y=propH, fill=Name)) + geom_bar(stat="identity", position=position_dodge()) + theme_bw() + scale_fill_brewer(palette = "Pastel1")
@@ -40,6 +79,7 @@ plotpHabitat <- function(pH) {
   return(plot)
 }
 
+#' PARA GRUPOS FUNCIONALES
 plotpFG <- function(pFG) {
 
   plot <- ggplot(pFG, aes(x=factor(FunctionalGroup), y=propFG, fill=Name)) + geom_bar(stat="identity", position=position_dodge()) + theme_bw() + scale_fill_brewer(palette = "Pastel1")
---
+++ README.md
@@ -0,0 +1,15 @@
+#Proporción de cada tipo de hábitat y grupo funcional de un ecosistema
+
+Paquete de R para calcular y graficar la proporción de los traits habitat y grupo funcional de las especies de un ecosistema
+
+##Dataset Example
+
+El archivo "traits.txt" contiene la clasificación de las especies del canal de Beagle y la caleta Potter recopilados por la autora de este paquete _Iara D. Rodriguez_, usando como base de los siguientes papers:
+
+  * Marina, T. I., Salinas, V., Cordone, G., Campana, G., Moreira, E., Deregibus, D., Momo, F. R. (2018). *The Food Web of Potter Cove (Antarctica): complexity, structure and function.* Estuarine, Coastal and Shelf Science, 200, 141–151. https://doi.org/10.1016/j.ecss.2017.10.015
+ 
+  * Riccialdelli, L., Newsome, S. D., Fogel, M. L., & Fernández, D. A. (2017). *Trophic interactions and food web structure of a subantarctic marine food web in the Beagle Channel: Bahía Lapataia, Argentina.* Polar Biology, 40(4), 807–821. https://doi.org/10.1007/s00300-016-2007-x
+  
+  *Kortsch, S., Primicerio, R., Fossheim, M., Dolgov, A. V, & Aschan, M. (2015). *Climate change alters the structure of arctic marine food webs due to poleward shifts of boreal generalists.* Proceedings of the Royal Society B: Biological Sciences, 282(1814), 20151546. https://doi.org/10.1098/rspb.2015.1546
+
+  *Jacob, U., Thierry, A., Brose, U., Arntz, W. E., Berg, S., Brey, T., Mollmann, C. (2011). *The Role of Body Size in Complex Food Webs. A Cold Case.* Advances in Ecological Research, 45, 181–223. https://doi.org/10.1016/B978-0-12-386475-8.00005-8
